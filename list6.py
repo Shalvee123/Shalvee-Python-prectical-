70cba921b8efdfcdf1b0dc9ecb95af054d45c7a8
Shalvee123 <shalveeyadav9@gmail.com>
2026-03-23 11:15:11 +0530

    Add files via upload

---
+++ list6.py
@@ -0,0 +1,17 @@
+#Programmed by- Shalvee Yadav
+#Roll No.-60
+#Practical No-06
+#Given a list of marks, find the len(), max(), and min() without using a loop.
+
+
+# List of marks
+marks = [45, 78, 90, 66, 82]
+
+# Find length, maximum, and minimum
+length = len(marks)
+maximum = max(marks)
+minimum = min(marks)
+
+print("Length:", length)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
