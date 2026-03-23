1e9cc74e8a2c177d4ea24cee95e504bb9c0c6207
Shalvee123 <shalveeyadav9@gmail.com>
2026-03-23 11:11:14 +0530

    Add files via upload

---
+++ list3.py
@@ -0,0 +1,15 @@
+#Programmed by- Shalvee Yadav
+#Roll No.-60
+#Practical No-03
+#Create a list of 6 numbers. Remove the 3rd element by index and the value 10 by name.
+# List of 6 numbers
+numbers = [5, 10, 15, 20, 25, 30]
+
+# Remove the 3rd element (index 2)
+numbers.pop(2)
+
+# Remove the value 10
+numbers.remove(10)
+
+# Print final list
+print(numbers)
