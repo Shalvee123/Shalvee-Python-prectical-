09540e22746f3ed98631ddf14467db9cc73d6fac
Shalvee123 <shalveeyadav9@gmail.com>
2026-03-23 11:12:07 +0530

    Add files via upload

---
+++ list4.py
@@ -0,0 +1,10 @@
+#Programmed by- Shalvee Yadav
+#Roll No.-60
+#Practical No-04
+#You have a list prices = [100, 200, 300]. Change the second price to 250
+prices = [100, 200, 300]
+
+# Change second price (index 1)
+prices[1] = 250
+
+print(prices)
