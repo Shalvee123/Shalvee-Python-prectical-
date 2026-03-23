bdc2686a2b8e998dbe39debed2498df1c94e3e7a
Shalvee123 <shalveeyadav9@gmail.com>
2026-03-23 11:10:50 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,15 @@
+#Programmed by- Shalvee Yadav
+#Roll No.-60
+#Practical No-02
+#Start with a list of 3 colors. Use .append() to add a fourth and .insert() to add one at the second position.
+# List of 3 colors
+colors = ["Red", "Blue", "Green"]
+
+# Add a fourth color using append()
+colors.append("Yellow")
+
+# Add a color at the second position (index 1) using insert()
+colors.insert(1, "Purple")
+
+# Print final list
+print(colors)
